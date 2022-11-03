b33254814f32e5477ed7dd3e487f3eb2ffe81eea
susil859clear <biswalsusil8@gmail.com>
2022-11-03 18:43:51 +0530

    i made some changes

--- Week2_Javascript/susiiil/myfirstscript.js
+++ Week2_Javascript/susiiil/myfirstscript.js
@@ -24,10 +24,10 @@ function change() {
 function validation() {
     var partyNO = document.getElementById('partyNo').value;
     if (!partyNO) {
-        document.getElementById('party').innerHTML = " please fil the party no.";
+        document.getElementById('party').innerHTML = "please enter party number";
     }
     else if (partyNO.length < 12 || partyNO.length > 22) {
-        document.getElementById('party').innerHTML = " please fil the correct party A/C no.";
+        document.getElementById('party').innerHTML = " in valid party number";
 
     }
     else if (isNaN(partyNO)) {
@@ -46,7 +46,7 @@ function validation() {
     if (!confirm) {
         document.getElementById('con').innerHTML = " confirm accout no cann't blank.";
     }
-    else if( partyNO != confirm) {
+    else if (partyNO != confirm) {
         document.getElementById('con').innerHTML = " confirm should same as party number";
 
     }
@@ -69,148 +69,159 @@ function validation() {
     }
     //ifsc validation
     let ifscCode = document.getElementById('ifsc_code').value;
-    let first_four =  ifscCode.substr(0,4);
-    let fifth_char =  ifscCode[4];
+    let first_four = ifscCode.substr(0, 4);
+    let fifth_char = ifscCode[4];
 
     if (!ifscCode) {
         document.getElementById('ifsc_valid').innerHTML = " enter the ifsc_code";
     }
-    
-    else if(ifscCode.length  < 11){
+
+    else if (ifscCode.length < 11) {
         document.getElementById('ifsc_valid').innerHTML = "it should be 11 character long";
     }
-    else if(first_four!= first_four.toUpperCase()){
+    else if (first_four != first_four.toUpperCase()) {
         document.getElementById('ifsc_valid').innerHTML = "it first four should be upper case";
     }
-    else if( fifth_char!=0){
+    else if (fifth_char != 0) {
         document.getElementById('ifsc_valid').innerHTML = "the fifth character should be zero";
 
     }
-    else{
+    else {
         document.getElementById('ifsc_valid').innerHTML = " ";
     }
     //head account validation
-    let headAccount= document.getElementById('head_account').value;
-    if(!headAccount){
+    let headAccount = document.getElementById('head_account').value;
+    if (!headAccount) {
         document.getElementById('hd').innerHTML = "plz enter head account";
     }
-    else{
+    else {
         document.getElementById('hd').innerHTML = "";
     }
     //purpose validatoon
-    var purpose=document.getElementById('purpose').value;
+    var purpose = document.getElementById('purpose').value;
     console.log('purpose')
-    if(!purpose){
+    if (!purpose) {
         document.getElementById('pur').innerHTML = "plz enter purpose";
     }
-   else if(purpose.length > 500){
-    document.getElementById('pur').innerHTML = "max 500 character allow";
-   }
-   else{
-    document.getElementById('pur').innerHTML = "";
-   }
-// party ammount vallidation
-let p_ammount=document.getElementById('p_ammount').value;
-console.log('p_ammount')
-if(!p_ammount){
-    document.getElementById('amt').innerHTML = "plz enter party ammount";
-}
-else{
-    document.getElementById('amt').innerHTML = "";
-}
+    else if (purpose.length > 500) {
+        document.getElementById('pur').innerHTML = "max 500 character allow";
+    }
+    else {
+        document.getElementById('pur').innerHTML = "";
+    }
+    // party ammount vallidation
+    let p_ammount = document.getElementById('p_ammount').value;
+    console.log('p_ammount')
+    if (!p_ammount) {
+        document.getElementById('amt').innerHTML = "plz enter party ammount";
+    }
+
+    else if (isNaN(p_ammount)) {
+        document.getElementById('amt').innerHTML = " party number should be number.";
+    }
+    else {
+        document.getElementById('amt').innerHTML = "";
+    }
 
 
 }
 
 function Scheck() {
     let ifscCode = document.getElementById('ifsc_code').value;
-    let first_four =  ifscCode.substr(0,4);
-    let fifth_char =  ifscCode[4];
+    let first_four = ifscCode.substr(0, 4);
+    let fifth_char = ifscCode[4];
     console.log('ifscCode')
-    if(ifscCode == 'SBIN0444555'){ 
+    if (ifscCode == 'SBIN0444555') {
         document.getElementById('bank_name').innerHTML = "SBIBANK";
         document.getElementById('bank_branch').innerHTML = "Bhubaneswar";
-    } 
-  
+    }
+
     else if (!ifscCode) {
         document.getElementById('ifsc_valid').innerHTML = " enter the ifsc_code";
-    }  
-    else if(first_four!= first_four.toUpperCase()){
+    }
+    else if (first_four != first_four.toUpperCase()) {
         document.getElementById('ifsc_valid').innerHTML = "it first four should be upper case";
     }
-    else if(ifscCode != 'SBIN0444555') {
-        document.getElementById('bank_name').innerHTML = "Wrong ifsc code";
-        document.getElementById('bank_branch').innerHTML = "wrong ifsc code";
-    } 
-    else if( fifth_char!=0){
+
+    else if (fifth_char != 0) {
         document.getElementById('ifsc_valid').innerHTML = "the fifth character should be zero";
 
     }
-  
+    else if (ifscCode.length < 11) {
+        document.getElementById('ifsc_valid').innerHTML = "it should be 11 character long";
+    }
+    else if (/[0-9]/.test(first_four)) {
+        document.getElementById('ifsc_valid').innerHTML = "the fIrst four digit should be alphabet";
+    }
+    else {
+        document.getElementById('ifsc_valid').innerHTML = " ";
+    }
+
+
 }
-function autofill(){
-    let headAccount= document.getElementById('head_account').value;
+function autofill() {
+    let headAccount = document.getElementById('head_account').value;
     console.log(' headAccount')
-    if (headAccount  == '0853001020002000000NVN'){
+    if (headAccount == '0853001020002000000NVN') {
         document.getElementById('bal').innerHTML = "1000000";
         document.getElementById('loc').innerHTML = "5000";
     }
-    else if (headAccount  == '8342001170004001000NVN'){
+    else if (headAccount == '8342001170004001000NVN') {
         document.getElementById('bal').innerHTML = "1008340";
         document.getElementById('loc').innerHTML = "4000";
     }
-   else if (headAccount  == '2071011170004320000NVN'){
+    else if (headAccount == '2071011170004320000NVN') {
         document.getElementById('bal').innerHTML = "14530000";
         document.getElementById('loc').innerHTML = "780000";
     }
-    else if (headAccount  == '8342001170004002000NVN'){
+    else if (headAccount == '8342001170004002000NVN') {
         document.getElementById('bal').innerHTML = "1056400 ";
         document.getElementById('loc').innerHTML = " 34000";
     }
-    else if (headAccount  == '2204000030006300303NVN'){
+    else if (headAccount == '2204000030006300303NVN') {
         document.getElementById('bal').innerHTML = " 123465400";
         document.getElementById('loc').innerHTML = "50000";
     }
-    else{
-        
-        document.getElementById('hd').innerHTML = "";
+    else {
+
+        document.getElementById('hd').innerHTML = ""; s
     }
 }
 //expt type...
-function fill(){
-    let exp= document.getElementById('exp_type').value;
+function fill() {
+    let exp = document.getElementById('exp_type').value;
     console.log('exp')
-    if (exp=='Capital Expenditure'){
-        document.getElementById('cap1').innerHTML=" Maintain current levels of operation within the organization"
-        document.getElementById('cap2').innerHTML=" Expenses to permit future expansion"
+    if (exp == 'Capital Expenditure') {
+        document.getElementById('cap1').innerHTML = " Maintain current levels of operation within the organization"
+        document.getElementById('cap2').innerHTML = " Expenses to permit future expansion"
     }
-    if (exp=='Capital Expenditure'){
-        document.getElementById('cap1').innerHTML=" Maintain current levels of operation within the organization"
-        document.getElementById('cap2').innerHTML=" Expenses to permit future expansion"
+    if (exp == 'Capital Expenditure') {
+        document.getElementById('cap1').innerHTML = " Maintain current levels of operation within the organization"
+        document.getElementById('cap2').innerHTML = " Expenses to permit future expansion"
     }
-    if (exp=='Revenue Expenditure'){
-        document.getElementById('cap1').innerHTML=" Sales costs or All expenses incurred by the firm"
-        document.getElementById('cap2').innerHTML="All expenses incurred by the firm to guarantee the smooth operation. "
+    if (exp == 'Revenue Expenditure') {
+        document.getElementById('cap1').innerHTML = " Sales costs or All expenses incurred by the firm"
+        document.getElementById('cap2').innerHTML = "All expenses incurred by the firm to guarantee the smooth operation. "
     }
-    if (exp=='Deferred Revenue Expenditure'){
-        document.getElementById('cap1').innerHTML="Exorbitant Advertising Expenditures"
-        document.getElementById('cap2').innerHTML="Unprecedented Losses"
-}
+    if (exp == 'Deferred Revenue Expenditure') {
+        document.getElementById('cap1').innerHTML = "Exorbitant Advertising Expenditures"
+        document.getElementById('cap2').innerHTML = "Unprecedented Losses"
+    }
 }
 
-let cD=document.getElementById('NavBar');
- let D=cD.className;
+let cD = document.getElementById('NavBar');
+let D = cD.className;
 console.log(cD);
-function changes(){
-cE=document.getElementById('NavBar');
-cF=document.getElementById('rightOne');
-if(D==cE.className){
-    cE.className='col-md-3 col-xs-3 d1';
-    cF.className='col-md-9 col-xs-9 d2';
-}
-else{
-    cE.className='col-md-3 col-xs-3 d-none d1';
-    cF.className='col-md-12 col-xs-9 d2';
-}
+function changes() {
+    cE = document.getElementById('NavBar');
+    cF = document.getElementById('rightOne');
+    if (D == cE.className) {
+        cE.className = 'col-md-3 col-xs-3 d1';
+        cF.className = 'col-md-9 col-xs-9 d2';
+    }
+    else {
+        cE.className = 'col-md-3 col-xs-3 d-none d1';
+        cF.className = 'col-md-12 col-xs-9 d2';
+    }
 }
 //part amount into words
