940cc61644cd78d6170fb4d8bb33bdf8776b4868
susil859clear <biswalsusil8@gmail.com>
2022-11-11 16:15:56 +0530

    week 3 task

--- Week3_Jquerytask/susil/mini.js
+++ Week3_Jquerytask/susil/mini.js
@@ -10,46 +10,60 @@ $('#button2').click(function () {
     $('#count').text(shopingCart.length)
     console.log(shopingCart)
 })
-let len=shopingCart.length
-$('#cart').click(function () {
+// let len=shopingCart.length
+// $('#cart').click(function () {
    
-   if(shopingCart.length!=0){
-    $('#cartdetails').removeClass('d-none');
+//    if(shopingCart.length!=0){
+//     $('#cartdetails').removeClass('d-none');
     
-    $('#itemcontainer').addClass('d-none')
+//     $('#itemcontainer').addClass('d-none')
 
     
-    for (let i = 0; i < shopingCart.length; i++) {
-        $('#cartdetails').append(`
-    <div class="col-md-4"  style="text-align: center">
-        <img src="${shopingCart[i].image}" style="width:400px;">
-        <div style="font-size:20px;">${shopingCart[i].name}</div>
+//     for (let i = 0; i < shopingCart.length; i++) {
+//         $('#cartdetails').append(`
+//     <div class="col-md-4"  style="text-align: center">
+//         <img src="${shopingCart[i].image}" style="width:400px;">
+//         <div style="font-size:20px;">${shopingCart[i].name}</div>
 
-        <div>$${shopingCart[i].price}</div>
-        <button type="button" class="btn btn-success" onclick="show()">Place Order</button>
-</div>
-   ` );
-    }
-    for(let j=0;j<len;j++){
-        shopingCart.pop()
-    }
+//         <div>$${shopingCart[i].price}</div>
+//         <button type="button" class="btn btn-success" onclick="show()">Place Order</button>
+// </div>
+//    ` );
+//     }
+//     for(let j=0;j<len;j++){
+//         shopingCart.pop()
+//     }
     
-   }
+//    }
    
-})
+// })
 // $('#placeOrder').removeclass('d-none')
 // $('#itemcontainer').addClass('d-none')
 // $('#placeOrder').click(function () {
 //     alert('order placed suceesfully')
 // })
-function show(){
-    alert('order placed suceesfully')
-}
-$('#home').click(function () {
-    $('#cartdetails').addClass('d-none');
-    $('#itemcontainer').removeClass('d-none')
+// function show(){
+//     alert('order placed suceesfully')
+// }
+// $('#home').click(function () {
+//     $('#cartdetails').addClass('d-none');
+//     $('#itemcontainer').removeClass('d-none')
    
     
-})
+// })
+$('#cart').click(function(){
+
+    const temp = JSON.stringify(shopingCart);
+    localStorage.setItem('cart_items',temp);
+    
+    for(let i=0;i<shopingCart.length;i++){
+    $('#output').append(`
+    <img src="${shopingCart[i].image}"margin-left:70px;">
+    <div style=" text-align: center;">${shopingCart[i].price}</div>
+    <div  style=" text-align: center;">${shopingCart[i].name}</div>
+    `);
+    
+    }
+    });
 
 
