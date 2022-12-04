509fd3fb6a6b03660bd90129ec2e99ca19569901
chinmaya <chinmayabiswal65@gmail.com>
2022-12-05 00:47:25 +0530

    final changes of project

--- Week6_Project/chinmaya/api/filterapi.php
+++ Week6_Project/chinmaya/api/filterapi.php
@@ -10,69 +10,49 @@ if(!$workingstatus && !$location && !$designation){
     echo json_encode($response);
     return;
 }
+
+
+$query="select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
+where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id ";
+
+
+
 if(!$location && !$designation){
-    $statement=$pdo->prepare("select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
-    where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id and e.working_status_id=?");
-    $statement->execute([$workingstatus]);
-    $result=$statement->fetchAll(PDO::FETCH_ASSOC);
-    $response=["status"=>true,"message"=>"data found","Data"=>$result];
-    echo json_encode($response);
-    return;
+    $query=$query.'and e.working_status_id='."{$workingstatus}";
+  
 }
-if(!$workingstatus && !$designation){
-    $statement=$pdo->prepare("select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
-    where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id and e.location_id=?");
-    $statement->execute([$location]);
-    $result=$statement->fetchAll(PDO::FETCH_ASSOC);
-    $response=["status"=>true,"message"=>"data found","Data"=>$result];
-    echo json_encode($response);
-    return;
+else if(!$workingstatus && !$designation){
+    $query=$query.'and e.location_id='."{$location}";
+   
 }
 
 
-if(!$workingstatus && !$location){
-    $statement=$pdo->prepare("select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
-    where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id and e.designation_id=?");
-    $statement->execute([$designation]);
-    $result=$statement->fetchAll(PDO::FETCH_ASSOC);
-    $response=["status"=>true,"message"=>"data found","Data"=>$result];
-    echo json_encode($response);
-    return;
+else if(!$workingstatus && !$location){
+
+    $query=$query.'and e.designation_id='.'{$designation}';
+    
 }
-if(!$workingstatus){
-    $statement=$pdo->prepare("select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
-    where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id and e.designation_id=? and e.location_id=?");
-    $statement->execute([$designation,$location]);
-    $result=$statement->fetchAll(PDO::FETCH_ASSOC);
-    $response=["status"=>true,"message"=>"data found","Data"=>$result];
-    echo json_encode($response);
-    return;
+else if(!$workingstatus){
+    $query=$query.'and e.designation_id='."{$designation}".'and e.location_id='.'{$location}';
+    
 }
 
-if(!$location){
-    $statement=$pdo->prepare("select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
-    where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id and e.designation_id=? and e.working_status_id=?");
-    $statement->execute([$designation,$workingstatus]);
-    $result=$statement->fetchAll(PDO::FETCH_ASSOC);
-    $response=["status"=>true,"message"=>"data found","Data"=>$result];
-    echo json_encode($response);
-    return;
+else if(!$location){
+
+    $query=$query.'and e.designation_id='.'{$designation}'."and e.working_status_id=".'{$workingstatus}';
+   
 }
 
-if(!$designation){
-    $statement=$pdo->prepare("select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
-    where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id and e.location_id=? and e.working_status_id=?");
-    $statement->execute([$location,$workingstatus]);
-    $result=$statement->fetchAll(PDO::FETCH_ASSOC);
-    $response=["status"=>true,"message"=>"data found","Data"=>$result];
-    echo json_encode($response);
-    return;
+else if(!$designation){
+    $query=$query.'and e.location_id='."{$location}".'and e.working_status_id'.'{$workingstatus}';
+    
 }
 
-
-$statement=$pdo->prepare("select e.id,concat(e.surename,' ',e.firstname,' ',e.lastname),e.date_of_joining,e.dob,e.gender,w.status_description,d.description,l.district,e.gross from employees as e,working_status as w,location as l,designation as d
-where e.working_status_id=w.id and e.location_id=l.id and e.designation_id=d.id and e.location_id=? and e.working_status_id=? and e.designation_id=? ");
-$statement->execute([$location,$workingstatus,$designation]);
+else{
+    $query=$query.'and e.location_id='.'{$location}'.'and e.working_status_id='.'{$workingstatus}'.'and e.designation_id='.'{$designation}';
+}
+$statement=$pdo->prepare($query);
+$statement->execute();
 $result=$statement->fetchAll(PDO::FETCH_ASSOC);
 $response=["status"=>true,"message"=>"data found","Data"=>$result];
 echo json_encode($response);
