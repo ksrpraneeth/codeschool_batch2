18352a730a59c5b7857a6b752a264f17f155bb38
chinmaya <chinmayabiswal65@gmail.com>
2022-12-06 18:13:02 +0530

    final project submit after the changes

--- Week6_Project/chinmaya/api/salary.php
+++ Week6_Project/chinmaya/api/salary.php
@@ -1,7 +1,7 @@
 <?php
 include "dbconnection.php";
 try{
-    $statement=$pdo->prepare("select concat(e.surename,' ',e.firstname,' ',e.lastname),s.month,s.year,s.paid_on,s.gross,s.deduction,s.net,s.employee_id from employees as e,salaries as s where e.id=s.employee_id");
+    $statement=$pdo->prepare("select concat(e.surename,' ',e.firstname,' ',e.lastname),s.month,s.year,to_char(s.paid_on,'DD-MM-YYYY')as paid_on,to_char(s.gross,'fm999G999D99')as gross,to_char(s.deduction,'fm999G999D99')as deduction,to_char(s.net,'fm999G999D99')as net,s.employee_id from employees as e,salaries as s where e.id=s.employee_id");
 
 
      $statement->execute();
